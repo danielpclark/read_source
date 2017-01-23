f02669f7005e324b0945991fe4a7383acccea331
Daniel P. Clark <6ftdan@gmail.com>
2017-01-23 15:09:25 -0500

    Add server support for VIM & keep Ruby running

--- lib/read_source/vim_source.rb
+++ lib/read_source/vim_source.rb
@@ -3,10 +3,16 @@
 module ReadSource
   module VimSource
     def vim servername=nil
-      file, line_num = send :source_location
+      (file, line_num = send :source_location) || return
       read_only = !!/#{ENV["GEM_HOME"]}/.match(file) ? "-M" : ""
       remote = "#{('--servername ' + servername.to_s) if servername} --remote-silent"
-      exec("vim #{remote} %s +%s %s" % [read_only, line_num, file]) if file
+      serverlist = `vim --serverlist`.split("\n")
+      if serverlist.include?(servername.to_s) || serverlist.include?("VIM")
+        `#{"vim #{remote} %s +%s %s" % [read_only, line_num, file]}` 
+        :success
+      else
+        exec("vim #{remote} %s +%s %s" % [read_only, line_num, file])
+      end
     end
   end
 end
